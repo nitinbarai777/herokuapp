1eec90375732e5e1b33007cb2d5203dfedc1e431
root <root@nitin.(none)>
2012-06-12 10:38:19 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herokuapp::Application.config.secret_token = 'ee674b87f9a724bedb0ed71bce988e9cb6aa4cdd9244b2bd07e58e9bb7c127302175c4a630b9dc68ff5185d1c1fa74d72ab079b63e4fe6135a5685e214ac1260'
